d016acf5b71492d0672b5a647d98c978904dcc74
Orangev4 <aaravikrishna30@gmail.com>
2024-08-31 01:16:41 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '7362034123:AAHIvgrnxoE_bgPhinknAJhbj7CMnUWZg0s'
+ADMIN_ID = '5184635355'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
